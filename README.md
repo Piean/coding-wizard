3a97c9c3f92b42b73bea2110678ce5ee8ddbae1f
yds <dishuang.yang@yunhuyj.com>
2020-09-09 15:05:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# coding-aide
+The plugin of idea
---
+++ src/main/java/com/piean/idea/plugin/codeing/view/BeanSetterAction.java
@@ -0,0 +1,16 @@
+package com.piean.idea.plugin.codeing.view;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author <a href="mailto:dishuang.yang@yunhuyj.com">mogu</a>
+ * @since 2020/9/8
+ */
+public class BeanSetterAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
